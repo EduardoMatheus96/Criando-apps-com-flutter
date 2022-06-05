6c598a0e366bfd1c0abac93abbc01db4099e2113
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2022-06-05 01:19:44 -0300

    Adding Slidable

--- Flutter 002/todo list/todo_list/lib/widgets/todo_list_item.dart
+++ Flutter 002/todo list/todo_list/lib/widgets/todo_list_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
+import 'package:flutter_slidable/flutter_slidable.dart';
 import '../models/todo.dart';
 
 class TodoListItem extends StatelessWidget {
@@ -10,30 +10,47 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
-        color: Colors.grey[200],
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 2),
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Slidable(
+      endActionPane: ActionPane(
+        extentRatio: 0.25,
+        motion: const ScrollMotion(),
         children: [
-          Text(
-            DateFormat('dd/MMM/yyyy - EE - HH:mm').format(todo.dateTime),
-            style: const TextStyle(
-              fontSize: 12,
-            ),
-          ),
-          Text(
-            todo.title,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
+          SlidableAction(
+            //padding: const EdgeInsets.all(16),
+            //flex: 2,
+            onPressed: (context) {},
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.remove,
+            label: 'Remove',
           ),
         ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: Colors.grey[200],
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 2),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MMM/yyyy - EE - HH:mm').format(todo.dateTime),
+              style: const TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            Text(
+              todo.title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
