888d94e7d1531879647782b338bbff049284e752
Eduardo Souto <eduardomatheusuesc@gmail.com>
2022-07-16 14:41:22 -0300

    Saving Todos

--- Lista Tarefas outra versao/lista_tarefas/lib/pages/lista_tarefas_page.dart
+++ Lista Tarefas outra versao/lista_tarefas/lib/pages/lista_tarefas_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
 class ListaTarefas extends StatefulWidget {
@@ -12,7 +13,47 @@ class ListaTarefas extends StatefulWidget {
 }
 
 class _ListaTarefasState extends State<ListaTarefas> {
+  final _toDoController = TextEditingController();
   List _todoList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _todoList = json.decode(data!);
+      });
+    });
+  }
+
+  Widget buildItem(context, index) {
+    return CheckboxListTile(
+      title: Text(_todoList[index]["title"]),
+      value: _todoList[index]["ok"],
+      secondary: CircleAvatar(
+        child: Icon(_todoList[index]["ok"] ? Icons.check : Icons.error),
+      ),
+      onChanged: (bool? value) {
+        setState(() {
+          _todoList[index]['ok'] = value;
+          _saveData();
+        });
+      },
+    );
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = {};
+      newToDo['title'] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo['ok'] = false;
+      _todoList.add(newToDo);
+      _saveData();
+    });
+  }
 
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
@@ -36,6 +77,56 @@ class _ListaTarefasState extends State<ListaTarefas> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: TextField(
+                    controller: _toDoController,
+                    decoration: const InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(color: Colors.blueAccent),
+                    ),
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _addToDo,
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blueAccent,
+                    padding: const EdgeInsets.all(15),
+                  ),
+                  child: const Text(
+                    "ADD",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.only(top: 10.0),
+              itemCount: _todoList.length,
+              itemBuilder: buildItem,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
