e3c67d41e157981e1778a1c178ca5e7ab6520fb7
Eduardo Souto <eduardomatheusuesc@gmail.com>
2022-07-19 16:02:16 -0300

    Correcao de bugs

--- Lista Tarefas outra versao/lista_tarefas/lib/pages/lista_tarefas_page.dart
+++ Lista Tarefas outra versao/lista_tarefas/lib/pages/lista_tarefas_page.dart
@@ -15,6 +15,8 @@ class ListaTarefas extends StatefulWidget {
 class _ListaTarefasState extends State<ListaTarefas> {
   final _toDoController = TextEditingController();
   List _todoList = [];
+  late Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos = 0;
 
   @override
   void initState() {
@@ -29,16 +31,55 @@ class _ListaTarefasState extends State<ListaTarefas> {
   }
 
   Widget buildItem(context, index) {
-    return CheckboxListTile(
-      title: Text(_todoList[index]["title"]),
-      value: _todoList[index]["ok"],
-      secondary: CircleAvatar(
-        child: Icon(_todoList[index]["ok"] ? Icons.check : Icons.error),
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: const Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
       ),
-      onChanged: (bool? value) {
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_todoList[index]["title"]),
+        value: _todoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_todoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (bool? value) {
+          setState(() {
+            _todoList[index]['ok'] = value;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
         setState(() {
-          _todoList[index]['ok'] = value;
+          _lastRemoved = Map.from(_todoList[index]);
+          _lastRemovedPos = index;
+          _todoList.removeAt(index);
+
           _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _todoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: const Duration(seconds: 2),
+          );
+          ScaffoldMessenger.of(context).removeCurrentSnackBar();
+          ScaffoldMessenger.of(context).showSnackBar(snack);
         });
       },
     );
@@ -53,6 +94,23 @@ class _ListaTarefasState extends State<ListaTarefas> {
       _todoList.add(newToDo);
       _saveData();
     });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(const Duration(seconds: 1));
+    setState(() {
+      _todoList.sort((a, b) {
+        if (a["ok"] && !b["ok"]) {
+          return 1;
+        } else if (!a["ok"] && b["ok"]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+      _saveData();
+    });
+    return null;
   }
 
   Future<File> _getFile() async {
@@ -119,11 +177,13 @@ class _ListaTarefasState extends State<ListaTarefas> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.only(top: 10.0),
-              itemCount: _todoList.length,
-              itemBuilder: buildItem,
-            ),
+            child: RefreshIndicator(
+                onRefresh: _refresh,
+                child: ListView.builder(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  itemCount: _todoList.length,
+                  itemBuilder: buildItem,
+                )),
           ),
         ],
       ),
