c678f465d17749be89a69b33c1ccf43789d4fa87
Eduardo Souto <eduardomatheusuesc@gmail.com>
2022-08-02 18:31:58 -0300

    backup

---
+++ Loja Virtual/loja_virtual/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/pages/home_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
